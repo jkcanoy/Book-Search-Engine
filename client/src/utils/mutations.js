cac5f33e1907746c79c40763d1471a30a0ec011a
Kyle Canoy <kcanoy@uw.edu>
2021-09-06 20:50:20 -0700

    create client side mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: bookInput!) {
+    addBook(bookInput: $bookInput) {
+      _id
+      email
+      username
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      email
+      username
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
